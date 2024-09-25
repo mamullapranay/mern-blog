3900628c7e77f910682c895ea4b72ab7fec3fb8d
pranay360 <mamullapranay360@gmail.com>
2024-09-25 20:17:17 +0530

    connected to database

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,18 +4,18 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About Pranay Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
+              Welcome to Pranay Blog! This blog was created by Sahand Ghavidel
               as a personal project to share his thoughts and ideas with the
               world. Sahand is a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
+              On this blog, you will find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
               languages. Sahand is always learning and exploring new
               technologies, so be sure to check back often for new content!
@@ -23,7 +23,7 @@ export default function About() {
 
             <p>
               We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
+              other readers. You can like other peoples comments and reply to
               them as well. We believe that a community of learners can help
               each other grow and improve.
             </p>
